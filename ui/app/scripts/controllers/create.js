253b14d80925ecd05fa7ebb3608d980e0ede9cf2
Dave Cassel <dmcassel@gmail.com>
2014-02-04 19:25:21 -0500

    Added some feature management

--- ui/app/scripts/controllers/create.js
+++ ui/app/scripts/controllers/create.js
@@ -12,11 +12,24 @@
           { name: 'Firefox', selected: false },
           { name: 'Chrome', selected: false },
           { name: 'IE', selected: false }
-        ]
+        ],
+        features: []
       };
 
       angular.extend($scope, {
-        model: model
+        model: model,
+        addFeature: function() {
+          if ($scope.model.features.indexOf($scope.model.selFeature) === -1) {
+            $scope.model.features.push($scope.model.selFeature);
+          }
+          $scope.model.selFeature = '';
+        },
+        removeFeature: function(feature) {
+          var index = $scope.model.features.indexOf(feature);
+          if (index !== -1) {
+            $scope.model.features.splice(index, 1);
+          }
+        }
       });
     }]);
 }());
