0903a16e321cfe9050a7f070cd1678c1cd28665d
Dave Cassel <dmcassel@gmail.com>
2014-02-12 08:48:26 -0500

    Initial cut at search service

--- ui/app/scripts/services/mlrest.js
+++ ui/app/scripts/services/mlrest.js
@@ -7,14 +7,28 @@
       this.$get = function($q, $http) {
         var service = {
           search: function() {
-
+            var d = $q.defer();
+            $http.get(
+              '/v1/search',
+              {
+                params: {
+                  format: 'json',
+                  options: 'all'
+                }
+              })
+            .success(function(data) {
+              d.resolve(data);
+            })
+            .error(function(reason) {
+              d.reject(reason);
+            });
+            return d.promise;
           },
           getDocument: function(uri) {
 
           },
           createDocument: function(doc, options) {
             // send a POST request to /v1/documents
-            console.log('create document');
             var d = $q.defer();
             $http.post(
               '/v1/documents',
