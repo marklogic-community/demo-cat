d2933db886d8bf47ba62e19f7092173b452801dd
Dave Cassel <dmcassel@gmail.com>
2014-02-13 08:58:32 -0500

    Enable editing of the description

--- ui/app/scripts/controllers/demo.js
+++ ui/app/scripts/controllers/demo.js
@@ -3,17 +3,35 @@
 
   angular.module('demoCat')
     .controller('DemoCtrl', ['$scope', 'MLRest', '$routeParams', function ($scope, mlRest, $routeParams) {
+      var uri = $routeParams.uri;
       var model = {
         // your model stuff here
+        edit: ''
       };
 
       console.log('DemoCtrl: uri=' + $routeParams.uri);
-      mlRest.getDocument($routeParams.uri).then(function(data) {
+      mlRest.getDocument(uri).then(function(data) {
         model.demo = data;
       });
 
       angular.extend($scope, {
-        model: model
+        model: model,
+        saveDesc: function() {
+          mlRest.patch(
+            uri,
+            {
+              "patch": [
+                {
+                  "replace": {
+                    "select": "$.description",
+                    "content": model.demo.description
+                  }
+                }
+              ]
+            }
+          );
+          model.edit = '';
+        }
       });
     }]);
 }());
