260f0551baf40992874682bde4d84b364cede08b
Dave Cassel <dmcassel@gmail.com>
2014-02-13 14:30:59 -0500

    removed example test

--- ui/test/spec/controllers/main.js
+++ ui/test/spec/controllers/main.js
@@ -16,7 +16,4 @@ describe('Controller: MainCtrl', function () {
     });
   }));
 
-  it('should attach a list of awesomeThings to the scope', function () {
-    expect(scope.model.awesomeThings.length).toBe(3);
-  });
 });
