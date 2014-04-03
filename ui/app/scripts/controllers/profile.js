ec2d4f7756266c18ce609177f64e99dc421a37d8
Geert Josten <geert.josten@marklogic.com>
2014-04-03 12:29:47 +0200

    Added profile functionality

--- ui/app/scripts/controllers/profile.js
+++ ui/app/scripts/controllers/profile.js
@@ -2,14 +2,28 @@
   'use strict';
 
   angular.module('demoCat')
-    .controller('ProfileCtrl', ['$scope', 'MLRest', function ($scope, mlRest) {
+    .controller('ProfileCtrl', ['$scope', 'MLRest', '$window', function ($scope, mlRest, win) {
       var model = {
-
+        user: {
+          name: '',
+          authenticated: false,
+          email: ''
+        }
       };
 
       angular.extend($scope, {
-        model: model
-
+        model: model,
+        submit: function() {
+          mlRest.createDocument($scope.model.user, {
+            format: 'json',
+            directory: '/users/',
+            extension: '.json',
+            'perm:demo-cat-role': 'read',
+            'perm:demo-cat-registered-role': 'update'
+          }).then(function(data) {
+            win.location.href = '/';
+          });
+        }
       });
     }]);
 }());
