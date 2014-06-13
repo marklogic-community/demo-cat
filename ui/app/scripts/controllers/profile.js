66d760ad1eaca55443f0635ab7de62fd62400097
Dave Cassel <dmcassel@gmail.com>
2014-06-13 11:26:45 -0400

    #56 Added a place for the new email address to live before it's added to the array

--- ui/app/scripts/controllers/profile.js
+++ ui/app/scripts/controllers/profile.js
@@ -4,13 +4,21 @@
   angular.module('demoCat')
     .controller('ProfileCtrl', ['$scope', 'MLRest', 'User', '$location', function ($scope, mlRest, user, $location) {
       var model = {
-        user: user // GJo: a bit blunt way to insert the User service, but seems to work
+        user: user, // GJo: a bit blunt way to insert the User service, but seems to work
+        newEmail: ''
       };
 
       angular.extend($scope, {
         model: model,
         addEmail: function() {
-          $scope.model.user.emails.push('');
+          if ($scope.profileForm.newEmail.$error.email) {
+            return;
+          }
+          if (!$scope.model.user.emails) {
+            $scope.model.user.emails = [];
+          }
+          $scope.model.user.emails.push(model.newEmail);
+          model.newEmail = '';
         },
         removeEmail: function(index) {
           $scope.model.user.emails.splice(index, 1);
