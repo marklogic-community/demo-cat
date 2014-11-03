63794d22d6f210c445413c8a6c39cfebc80d96b2
Geert Josten <geert.josten@marklogic.com>
2014-11-03 22:12:31 +0100

    Added init scripts for demo-cat

---
+++ etc/prod/conf.sh
@@ -0,0 +1,25 @@
+
+# Define overrides for init script defaults here, defaults shown in comments..
+
+# name of the project
+#NAME=${0##*/}
+
+# location of your project's root
+SOURCE_DIR=/space/projects/$NAME.live
+
+# js entry point
+#SOURCE_FILE=startup.js
+
+# server script to be launched
+#SERVER_SCRIPT=./server.js
+
+# port at which node server should be listening
+#APP_PORT=9040
+APP_PORT=9070
+
+# MarkLogic host to which node server should proxy
+#ML_HOST=localhost
+
+# MarkLogic port to which node server should proxy
+#ML_PORT=8040
+ML_PORT=8020
