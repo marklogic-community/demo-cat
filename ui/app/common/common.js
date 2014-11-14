d0bd7a6236d8722e1cf0aadc3c5b8e863437330a
Ryan Dew <ryan.j.dew@gmail.com>
2014-11-13 21:31:42 -0700

    Update to latest from slush generator, #75

--- ui/app/common/common.js
+++ ui/app/common/common.js
@@ -1,2 +1,14 @@
 
-angular.module('demoCat.common', []);
+angular.module('demoCat.common', [])
+  .filter('object2Array', function() {
+    'use strict';
+
+    return function(input) {
+      var out = [];
+      for (var name in input) {
+        input[name].__key = name;
+        out.push(input[name]);
+      }
+      return out;
+    };
+});
