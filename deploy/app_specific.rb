ec2f1e1a83fd29d50d2aa8674f416c906f420bf4
Geert Josten <geert.josten@marklogic.com>
2015-01-30 22:08:38 +0100

    Fixed deploy rest to bypass external security

--- deploy/app_specific.rb
+++ deploy/app_specific.rb
@@ -10,7 +10,7 @@ class ServerConfig
     original_deploy_modules
     r = execute_query %Q{
       xquery version "1.0-ml";
-
+      
       for $uri in cts:uris()
       return (
         $uri,
@@ -23,11 +23,80 @@ class ServerConfig
     { :db_name => @properties["ml.modules-db"] }
   end
 
+  # aks for ldap credentials
+  alias_method :original_bootstrap, :bootstrap
+  def bootstrap()
+    if (@properties["ml.external-security"] != "")
+      if (@properties["ml.ldap-user"] == "") then
+        if STDIN.respond_to?(:noecho)
+        print "Enter an LDAP user: "
+        @properties["ml.ldap-user"] = STDIN.noecho(&:gets).chomp
+        print "\n"
+        else
+          raise ExitException.new("Upgrade to Ruby >= 1.9 for prompting on the shell.")
+        end
+      end
+      
+      if (@properties["ml.ldap-password"] == "") then
+        if STDIN.respond_to?(:noecho)
+        print "Enter an LDAP password: "
+        @properties["ml.ldap-password"] = STDIN.noecho(&:gets).chomp
+        print "\n"
+        else
+          raise ExitException.new("Upgrade to Ruby >= 1.9 for prompting on the shell.")
+        end
+      end
+    end
+    
+    original_bootstrap
+  end
+  
+  # adjust REST security to allow deploy
+  alias_method :original_deploy_rest, :deploy_rest
+  def deploy_rest()
+    if (@properties["ml.internal-security"] == "false")
+      r = execute_query %Q{
+        xquery version "1.0-ml"; 
+      
+        import module namespace admin = "http://marklogic.com/xdmp/admin" at "/MarkLogic/admin.xqy";
+      
+        let $config := admin:get-configuration()
+        let $config := admin:appserver-set-internal-security($config, xdmp:server("#{@properties["ml.app-name"]}"), fn:true())
+        return
+          admin:save-configuration-without-restart($config)
+      },
+      { :app_name => @properties["ml.app-name"] }
+    end
+    
+    original_deploy_rest
+    
+    if (@properties["ml.internal-security"] == "false")
+      r = execute_query %Q{
+        xquery version "1.0-ml"; 
+      
+        import module namespace admin = "http://marklogic.com/xdmp/admin" at "/MarkLogic/admin.xqy";
+      
+        let $config := admin:get-configuration()
+        let $config := admin:appserver-set-internal-security($config, xdmp:server("#{@properties["ml.app-name"]}"), fn:false())
+        return
+          admin:save-configuration-without-restart($config)
+      },
+      { :app_name => @properties["ml.app-name"] }
+    end
+  end
+  
+  def migrate_data()
+    fix_permissions()
+    fix_credentials()
+    fix_contacts()
+  end
+  
   # fix content permissions
   def fix_permissions()
+    logger.info "Fixing permissions.."
     r = execute_query %Q{
       xquery version "1.0-ml";
-
+      
       for $uri in cts:uris()
       return (
         $uri,
@@ -38,6 +107,36 @@ class ServerConfig
         ))
       )
     },
-    { :db_name => @properties["ml.content-db"] }
+    { :app_name => @properties["ml.app-name"] }
+    
+    r.body = parse_json r.body
+    logger.debug r.body
   end
+  
+  def fix_credentials()
+    logger.info "Fixing credentials.."
+    r = execute_query %Q{
+      xquery version "1.0-ml";
+      
+      xdmp:invoke("/data-transforms/add-credentials.xqy")
+    },
+    { :app_name => @properties["ml.app-name"] }
+    
+    r.body = parse_json r.body
+    logger.info r.body
+  end
+  
+  def fix_contacts()
+    logger.info "Fixing contacts.."
+    r = execute_query %Q{
+      xquery version "1.0-ml";
+      
+      xdmp:invoke("/data-migration/maintainer-to-tech-contact.xqy")
+    },
+    { :app_name => @properties["ml.app-name"] }
+    
+    r.body = parse_json r.body
+    logger.info r.body
+  end
+  
 end
