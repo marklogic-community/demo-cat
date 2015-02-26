0a6a8f3694acc16b8e5a6a16ac194c2a01d8b417
Matt Pileggi <Matt.Pileggi@marklogic.com>
2015-02-26 11:30:02 -0500

    Issue #133 - provide short description
    include ellipses if truncated

--- ui/app/search/search.js
+++ ui/app/search/search.js
@@ -1,2 +1,21 @@
+'use strict';
+angular.module('demoCat.search', [])
+  .filter('searchsummary', function() {
+    return function(input) {
+      var t = '';
+      // make sure it has a non-empty short description value
+      if (input.shortdesc && input.shortdesc.values && input.shortdesc.values.length && input.shortdesc.values[0]) {
+        t = input.shortdesc.values[0];
+      } else {
 
-angular.module('demoCat.search', []);
+        var desc = (input.description && input.description.values && input.description.values[0]) || '';
+        // it's likely rich html so we need to get text only
+        var dummydiv = window.jQuery('<div></div>').html(desc);
+        t = dummydiv.text();
+        if (t.length > 95) {
+          t = t.substring(0,95) + '...';
+        }
+      }
+      return t;
+    };
+  });
