a2bec19d4c67c11a310db23eba993288a059efc5
David Grant <dave.grant@marklogic.com>
2015-02-26 12:59:19 -0500

    #91 - adding demo status

--- deploy/app_specific.rb
+++ deploy/app_specific.rb
@@ -224,6 +224,20 @@ class ServerConfig
           <alert:options/> )
       return alert:action-insert("http://marklogic.com/demo-cat/notifications", $action);
 
+      (: Connect the "demo broken" action to the alerting configuration :)
+      xquery version "1.0-ml";
+      import module namespace alert = "http://marklogic.com/xdmp/alert" at "/MarkLogic/alert.xqy";
+
+      let $action2 := alert:make-action(
+          "demo-broken-action",
+          "Action to execute when a demo is marked as broken.",
+          xdmp:database('#{@properties["ml.modules-db"]}'),
+          '/',
+          "/alerting/alert-demo-broken-action.xqy",
+          <alert:options/> )
+      return alert:action-insert("http://marklogic.com/demo-cat/notifications", $action2);
+
+
       (: Consider adding any one-off Alerting rules here. :)
 
       },
