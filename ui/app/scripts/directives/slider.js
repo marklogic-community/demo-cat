67b2d9efd9c9aa71899c023c6708f15ad7d2e549
jenbreese <jen@evilgirlnextdoor.com>
2015-04-13 12:04:48 -0700

    homepage slider

--- ui/app/scripts/directives/slider.js
+++ ui/app/scripts/directives/slider.js
@@ -1,9 +1,41 @@
-var sliderApp=angular.module('sliderApp',['ngAnimate']);
+var sliderApp=angular.module('sliderApp',[]);
 
 sliderApp.controller('SliderController', function($scope) {
     'use strict';
 
-    $scope.images=[{src:'situational-wareness-sm.png',title:'Situational Awareness'},{src:'ediscovery-sm.png',title:'eDiscovery'},{src:'dmlchealthcare-sm.png',title:'DMLC Healthcare'},{src:'obi-sm.png',title:'OBI'}]; 
+    $scope.images=[
+    {
+        src:'situational-wareness-sm.png',
+        title:'Situational Awareness',
+        uri:'http://catalog.demo.marklogic.com/detail/demos/17784760062083407114.json',
+        desc:'This is a short description.'
+    },
+    {
+        src:'ediscovery-sm.png',
+        title:'eDiscovery',
+        uri:'http://catalog.demo.marklogic.com/detail/demos/5462875041093986835.json',
+        desc:'A Day in the life of an investigator @CITI.'
+    },
+    {
+        src:'dmlchealthcare-sm.png',
+        title:'DMLC Healthcare',
+        uri:'http://www.cnn.com',
+        desc:'This is a short description.'
+    },
+    {
+        src:'obi-sm.png',
+        title:'OBI',
+        uri:'https://wiki.marklogic.com/display/PUBLICSOLN/Backup+Object+Based+Intelligence',
+        desc:'The MarkLogic OBP solution answers this challenge by providing tradecraft tools and a data model capable of storing, enhancing and disseminating high value analysis products. '
+    },
+    {
+        src:'exec-paycheck.png',
+        title:'Executive Paycheck',
+        uri:'https://wiki.marklogic.com/display/CS/Media+Demo',
+        desc:'This site collects information about executive compensation from SEC filings and presents them as an information application that lets users search, browse and analyse the data and gain insight into the pay structure of the Fortune 500.'
+    }
+
+    ]; 
 });
  
 sliderApp.directive('slider', function ($timeout) {
@@ -26,6 +58,7 @@ sliderApp.directive('slider', function ($timeout) {
         scope.prev=function(){
             scope.currentIndex>0?scope.currentIndex--:scope.currentIndex=scope.images.length-1;
         };
+
         
         scope.$watch('currentIndex',function(){
             scope.images.forEach(function(image){
@@ -50,7 +83,15 @@ sliderApp.directive('slider', function ($timeout) {
         scope.$on('$destroy',function(){
             $timeout.cancel(timer);
         });
+
+        scope.pause=function(){
+            $timeout.cancel(timer);
+        };
         
+        scope.play=function(){
+            sliderFunc();
+        };
+
         /* End : For Automatic slideshow*/
         angular.element(document.querySelectorAll('.arrow')).one('click',function(){
             $timeout.cancel(timer);
