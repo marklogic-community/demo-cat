23ab29a3d0a123de8ee8d6091195f48c24f12f37
Geert Josten <geert.josten@marklogic.com>
2015-04-21 20:52:36 +0200

    Enabled edit modals with stub contents for home

--- ui/app/home/home-ctrl.js
+++ ui/app/home/home-ctrl.js
@@ -2,10 +2,65 @@
   'use strict';
   angular.module('demoCat.home', []);
   angular.module('demoCat.home')
-    .controller('HomeCtrl', ['$scope', function ($scope) {
-      var model = {};
+    .controller('HomeCtrl', ['$scope', '$modal', '$sce', 'user', function ($scope, $modal, $sce, user) {
+      var model = { user: user };
       angular.extend($scope,{
-        model: model
+        model: model,
+        editVanguard: editVanguard,
+        editDontTouch: editDontTouch,
+        editSpotlight: editSpotlight
       });
+      
+      function editVanguard() {
+        showModal('/views/modals/edit-vanguard.html', 'Edit Vanguard', model);
+      }
+      
+      function editDontTouch() {
+        showModal('/views/modals/edit-dont-touch.html', 'Edit Don\'t Touch', model);
+      }
+      
+      function editSpotlight() {
+        showModal('/views/modals/edit-spotlight.html', 'Edit Spotlight', model);
+      }
+      
+      function showModal(template, title, model, validate) {
+        return $modal.open({
+          templateUrl: template+'',
+          controller: function ($scope, $modalInstance, title, model, validate, user) {
+            $scope.title = title;
+            $scope.model = model;
+            $scope.user = user;
+            $scope.alerts = [];
+            $scope.ok = function () {
+              if (validate) {
+                $scope.alerts = validate($scope.model);
+              }
+              if ($scope.alerts.length === 0) {
+                $modalInstance.close($scope.model);
+              }
+            };
+            $scope.cancel = function () {
+              $modalInstance.dismiss('cancel');
+            };
+            $scope.encodeURIComponent = encodeURIComponent;
+            $scope.trustUrl = $sce.trustAsResourceUrl;
+          },
+          size: 'lg',
+          resolve: {
+            title: function () {
+              return title;
+            },
+            model: function () {
+              return model;
+            },
+            validate: function () {
+              return validate;
+            },
+            user: function () {
+              return user;
+            }
+          }
+        }).result;
+      }
     }]);
 }());
