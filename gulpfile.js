6f9104175e99442473969f01914bc1933cf35840
Geert Josten <geert.josten@marklogic.com>
2015-04-22 13:38:39 +0200

    Made home content and edits dynamic

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ gulp.task('jshint', function() {
 
 // Compile Our Less
 gulp.task('less', function() {
-  return gulp.src('ui/app/styles/main.less')
+  return gulp.src('ui/app/styles/*.less')
     .pipe(less())
     .pipe(gulp.dest('ui/app/styles/'));
 });
