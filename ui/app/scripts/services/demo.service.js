f4b509319224fe11698354a0c10a4f3d4f6b0684
Ryan Dew <ryan.j.dew@gmail.com>
2015-04-22 22:33:25 -0600

    Prep work for demo suggest and save vanguard section

--- ui/app/scripts/services/demo.service.js
+++ ui/app/scripts/services/demo.service.js
@@ -31,6 +31,34 @@
       return upload(demo, file, '/demo/update?' + $.param(updateParams));
     }
 
+    function suggestDemo(demoSearchText) {
+      var updateParams = angular.extend({uri: docUri}, params);
+      return $http.post('v1/values/name-uri', {
+        query: {
+          'queries': [{
+            'word-query': {
+              'element': {
+                'name': 'name',
+                'ns': ''
+              },
+              'text': [demoSearchText + '*'],
+              'term-option': ['wildcarded', 'case-insensitive']
+            }
+          }]
+        }
+      }, { params: {options: 'all'}}).then(function(response){
+        var demos = [];
+        angular.forEach(response.data['values-response'].tuple, function(){
+          var values = value['distinct-value'];
+          demos.push({
+            name: values[0]._value,
+            uri: values[1]._value
+          });
+        });
+        return demos;
+      });
+    }
+
     function upload(demo, file, url) {
       return $upload.upload({
           url: url,
